859f0f6a1fa35f49351fbf372a82c47a4562d062
PrithviS18 <prithvisen003@gmail.com>
2025-05-31 01:11:22 +0530

    client changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import store from './store/appStore';
-import { SocketProvider } from './context/socketContext.jsx';
+import { SocketProvider } from './context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
